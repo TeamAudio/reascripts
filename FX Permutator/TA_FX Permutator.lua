ecead4cbe79aca26db079ce1cc6a9406bd53c553
Dave Benjamin <dave@teamaud.io>
2023-10-17 10:29:36 -0700

    Update FX Permutator

--- FX Permutator/TA_FX Permutator.lua
+++ FX Permutator/TA_FX Permutator.lua
@@ -1,12 +1,11 @@
 -- @description FX Permutator
--- @version 1.1.0
+-- @version 1.2.0
 -- @changelog
---   HiDPI/Retina support
---   AU plugin support
---   Plugin priority option
---   Usability improvements
---   Performance optimization
+--   REAPER 7 support
 -- @author Tech Audio
--- @metapackage
 -- @provides
---   [main] TA_FX Permutator.luac > TA_FX Permutator.lua
+--   TA_FX Permutator-5.3.dat
+--   TA_FX Permutator-5.4.dat
+
+script_path, script_name = ({reaper.get_action_context()})[2]:match("(.-)([^/\\]+).lua$")
+loadfile(script_path .. 'TA_FX Permutator-' .. _VERSION:match('[%d.]+') .. '.dat')()
