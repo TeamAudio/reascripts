c1ee347ae57e49d6128b747c38ec498ce75c0704
Roel <roel@teamaud.io>
2023-12-06 21:50:50 -0600

    Update FX Permutator

--- FX Permutator/TA_FX Permutator.lua
+++ FX Permutator/TA_FX Permutator.lua
@@ -1,7 +1,13 @@
 -- @description FX Permutator
--- @version 1.2.0
+-- @version 1.3.0
 -- @changelog
---   REAPER 7 support
+--   Parallel Chance % feature allows root level FX of run to be run in parallel.
+--   FX Containers: New Options menu tab allows you to create FX Container as part of your FX Permutator run with their own unique parameters.
+--   New FX Container specific actions in the Tools menu 
+--   - Randomize Container Params 
+--   - Randomize Container Presets
+--   - Shuffle Container FX
+--    Additional Logging for plugin load failures
 -- @author Tech Audio
 -- @provides
 --   TA_FX Permutator-5.3.dat
