2b57646ad5e6270f6447abbf03e0deeece560ef0
Dave Benjamin <dave@teamaud.io>
2023-12-31 10:35:43 -0700

    Update FX Permutator

--- FX Permutator/TA_FX Permutator.lua
+++ FX Permutator/TA_FX Permutator.lua
@@ -1,13 +1,9 @@
 -- @description FX Permutator
--- @version 1.3.0
+-- @version 1.3.1
 -- @changelog
---   Parallel Chance % feature allows root level FX of run to be run in parallel.
---   FX Containers: New Options menu tab allows you to create FX Container as part of your FX Permutator run with their own unique parameters.
---   New FX Container specific actions in the Tools menu 
---   - Randomize Container Params 
---   - Randomize Container Presets
---   - Shuffle Container FX
---    Additional Logging for plugin load failures
+--   General UI improvements
+--   Selected FX filter
+--   Activation fix for Windows
 -- @author Tech Audio
 -- @provides
 --   TA_FX Permutator-5.3.dat
