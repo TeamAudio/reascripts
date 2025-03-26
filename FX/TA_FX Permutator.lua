5988d861bc517d2448212fc71123ecff96b1214c
Michael DeFreitas <mikeylove@gmail.com>
2025-03-26 09:34:22 -0700

    move FX Permutator to FX directory (category)

--- FX/TA_FX Permutator.lua
+++ FX/TA_FX Permutator.lua
@@ -6,8 +6,8 @@
 --   Activation fix for Windows
 -- @author Tech Audio
 -- @provides
---   TA_FX Permutator-5.3.dat
---   TA_FX Permutator-5.4.dat
+--   fxperm/TA_FX Permutator-5.3.dat
+--   fxperm/TA_FX Permutator-5.4.dat
 
 script_path, script_name = ({reaper.get_action_context()})[2]:match("(.-)([^/\\]+).lua$")
 loadfile(script_path .. 'TA_FX Permutator-' .. _VERSION:match('[%d.]+') .. '.dat')()
