2a8eadc8a9c1b4159f94fb6b66d61445b5dbf6b4
Michael DeFreitas <mikeylove@gmail.com>
2025-03-26 09:40:57 -0700

    update load path for fxperm luac

--- FX/TA_FX Permutator.lua
+++ FX/TA_FX Permutator.lua
@@ -10,4 +10,4 @@
 --   fxperm/TA_FX Permutator-5.4.dat
 
 script_path, script_name = ({reaper.get_action_context()})[2]:match("(.-)([^/\\]+).lua$")
-loadfile(script_path .. 'TA_FX Permutator-' .. _VERSION:match('[%d.]+') .. '.dat')()
+loadfile(script_path .. 'fxperm/TA_FX Permutator-' .. _VERSION:match('[%d.]+') .. '.dat')()
